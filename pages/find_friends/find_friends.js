0c02095b28bed81b97fe31e8e3186af1b68d99f3
Imani-ES <imanimuh@buffalo.edu>
2021-10-13 12:54:49 -0400

    connecting to database #63
    Base code of connecting to database.
    Does not run.

--- pages/find_friends/find_friends.js
+++ pages/find_friends/find_friends.js
@@ -8,6 +8,29 @@ function find_User() {
     }
     else{
         document.getElementById("Search_popup").innerHTML = "";
-        //
+        // send user_search to the server
+        var user_search = search.innerHTML;
+        
     }
 }
+
+const { MongoClient } = require("mongodb");
+// Replace the uri string with your MongoDB deployment's connection string.
+const uri =
+  "mongodb+srv://<user>:<password>@<cluster-url>?retryWrites=true&writeConcern=majority";
+const client = new MongoClient(uri);
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db('');//Users
+    const global_users = database.collection('');//Global Users
+    // Query for user_search
+    const query = { username: user_search };// username: 
+    const movie = await movies.findOne(query);
+    console.log(movie);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
