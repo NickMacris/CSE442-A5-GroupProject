bfe97c5e4378ab481d223b3a7175064874abe60d
NickMacris <Nick.Macris@yahoo.com>
2021-10-16 22:21:35 -0400

    #56 Added button in profilepage2 that inserts horror genre into existing user on database

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const path = require("path");
 const port = process.env.PORT || 3000;
 //require('./simpleWebpage/database');
 
+let MongoClient = require('mongodb').MongoClient;
+let dbPass = "CSE442cse";
+let url = 'mongodb+srv://CSE442:' + dbPass + '@cluster0.k7tia.mongodb.net/test';
+
+
 app.use(express.static('public'));
 app.use('/css', express.static(__dirname + 'public/css'));
 
@@ -18,11 +23,27 @@ app.get('/style.css', (req, res) => {
 })
 
 app.get('/profile', (req, res) => {
-    res.sendFile(path.join(__dirname, 'profilePage.html')) ;
+    res.sendFile(path.join(__dirname, 'profilePage2.html')) ;
 })
 app.get('/styleProfile.css', (req, res) => {
     res.sendFile(path.join(__dirname, 'styleProfile.css'));
 })
 app.listen(port, () => {
     console.log(`App is running on ${port}`)
+});
+app.post('/add_genre', (req, res) => {
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        let dbo = db.db("SimpleTest");
+        let myobj = {name: "Nick Inc", address: "Highway 32"};
+        dbo.collection("documents").updateOne(
+            { user: "User1" },
+            { $push: { genres: "Horror" } },
+        )
+        /*dbo.collection("documents").insertOne(myobj, function (err, res) {
+            if (err) throw err;
+            console.log("1 document inserted");
+            db.close();
+        });*/
+    });
 });
