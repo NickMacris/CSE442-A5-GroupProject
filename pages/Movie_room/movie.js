099ed0d1999477574b076e2f886db1f5e7093a49
Imani-ES <imanimuh@buffalo.edu>
2021-11-05 12:20:29 -0400

    setup database interface #82
    *Does not run*

--- pages/Movie_room/movie.js
+++ pages/Movie_room/movie.js
@@ -1,27 +1,37 @@
-$(function () {
+    var chat = document.getElementById('input_chat');
+    var chat_history = document.getElementById('chat_history');
+    var booth = document.getElementById('voting_booth');
 
-    var chat = $('#input_chat');
-    var chat_history = $('#chat_history');
-    var booth = $('#voting_booth');
+    function vote_yes(){
 
+    }
+
+    function vote_no(){
+      console.log('Vote Recieved')
+    }
+
+    function send_chat(){
+      console.log('Sending Message');
+    }
+
+
+
+//Web socket stuff
     // if user is running mozilla then use it's built-in WebSocket
     window.WebSocket = window.WebSocket || window.MozWebSocket;
     // if browser doesn't support WebSocket, just show
     // some notification and exit
     if (!window.WebSocket) {
     console.log('Browser does not support WebSocket.');
-    return;
     }
     var connection = new WebSocket('ws://127.0.0.1:1337');
     
     connection.onopen = 
     function () {
         // Greet User
-        booth.html(
-            $('<p>',{text: 'Welcome !! Voting will begin when all users enter room.'}));
+        booth.innerHTML += '<p> Welcome !! Voting will begin when all users enter room.</p>';
             //loop through chat history, and 
-        chat_history.html(
-            $('<p>', {text: 'Chatting.'}));
+        chat_history.innerHTML += '<p> Chatting </p>';
     };
   
     connection.onerror = function (error) {
@@ -42,8 +52,13 @@ $(function () {
       // handle incoming message
       if (json.type === 'movie'){
           var movie_name = json.data[0];
-        booth.html(
-            $('<p>',{text: movie_name}));
+        booth.innerHTML = movie_name;
       }
-    };
-});
+      if (json.type === 'chat_history'){
+        for(chat in json.data){
+          chat_history.innerHTML +=json.data[chat][0]+": "+json.data[chat][1] +'<br>';
+          console.log(json.data[chat]);
+        }
+        
+      }
+    }
--- pages/Movie_room/server.js
+++ pages/Movie_room/server.js
@@ -1,52 +1,89 @@
-var WebSocketServer = require('websocket').server;
-var http = require('http');
-
-var server = http.createServer(function(request, response) {
-  // process HTTP request. Since we're writing just WebSockets
-  // server we don't have to implement anything.
-});
+//database init
+    const { MongoClient } = require("mongodb");
+    const { WSATYPE_NOT_FOUND } = require('constants');
+    const imani_dbPass = process.env.DB_PASS_442;
+    const imani_uri = 'mongodb+srv://CSE442:' + 'CSE442cse' + '@cluster0.k7tia.mongodb.net/test';
+    const imani_client = new MongoClient(imani_uri,{keepAlive: 1});
 
 //Global variables
-var skt_port = 1337;
-var clients = [];
-var movie_list = [["King Kong",0],["Godzilla",0],["Chicken Run",0],["Ratatowey",0]];
-var chat_history = [];
-var room_size = 3; // Get amount of people in room from db
+    var skt_port = 1337;
+    var clients = [];
+    var movie_list = [["King Kong",0],["Godzilla",0],["Chicken Run",0],["Ratatowey",0]];
+    var chat_history = [["User_1","Hello"],["User_2","Goodbye"]];
+    var room_size = 3; // Get amount of people in room from db
 
-server.listen(skt_port, function() { 
-    console.log((new Date()) + ", Server is now listening on port: " + skt_port);
-});
-
-// create the server
-wsServer = new WebSocketServer({
-  httpServer: server
-});
-
-// WebSocket server
-wsServer.on('request', function(request) {
-    console.log((new Date()) + ", Connection from origin: " + request.origin +".");
-    var connection = request.accept(null, request.origin);
-    var client = clients.push(connection) -1;
-    
-    if(movie_list.length >0){
-        connection.sendUTF(
-            JSON.stringify({type: 'movie', data: movie_list[0]}));
-    }
-
-    if(chat_history.length > 0){
-        connection.sendUTF(
-            JSON.stringify({type: 'chat_history', data: chat_history}));
-    }
-
-    // This is the most important callback for us, we'll handle
-    // all messages from users here.
-    connection.on('message', function(message) {
-    if (message.type === 'utf8') {
-        // process WebSocket message
-    }
+//websocket init
+    var WebSocketServer = require('websocket').server;
+    var http = require('http');
+    var server = http.createServer(function(request, response) {
+    // process HTTP request. Since we're writing just WebSockets
+    // server we don't have to implement anything.
+    });
+    server.listen(skt_port, function() { 
+        console.log((new Date()) + ", Server is now listening on port: " + skt_port);
+    });
+    wsServer = new WebSocketServer({
+    httpServer: server
     });
 
-    connection.on('close', function(connection) {
-    // close user connection
+// Server functionality
+    wsServer.on('request', function(request) {
+        console.log((new Date()) + ", Connection from origin: " + request.origin +".");
+        var connection = request.accept(null, request.origin);
+        var client = clients.push(connection) -1;
+        console.log('User #' + client+' has been added');
+
+        if(chat_history.length > 0){
+            connection.sendUTF(
+                JSON.stringify({type: 'chat_history', data: chat_history}));
+        }
+
+        if(client >= room_size){
+            console.log("Begin Voting");
+            vote();
+        }
+        // This is the most important callback for us, we'll handle
+        // all messages from users here.
+        connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            // process WebSocket message
+        }
+        });
+
+        connection.on('close', function(connection) {
+        // close user connection
+        });
     });
-});
+
+//helper funcitons
+/**
+ * Vote() should send voting related client messages
+ */
+    function vote(){
+        connection.sendUTF(
+            JSON.stringify({type:'vote',data:'Begin'})
+        )
+        
+        if(movie_list.length >0){
+            connection.sendUTF(
+                JSON.stringify({type: 'movie', data: movie_list[0]}));
+        }
+    }
+
+//database interfaces
+/**
+ * Should retrieve room movie list and objects, and user list.
+ * Not completed!!
+ */
+    async function get_room_info(name,res) {
+        await imani_client.connect();
+        console.log("MongoDB connected");
+        console.log(name);
+        const db = imani_client.db("UserInfo");
+        const global_users = db.collection('username');//Global Users
+        // Users are stored as [{username: "Username"},{password,"pass"}]
+        global_users.findOne({username:name},{}, function(err, result) {
+            if (err) throw err;
+            console.log(result);
+        }); 
+    }
